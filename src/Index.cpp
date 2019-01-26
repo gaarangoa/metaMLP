561c3cde79d8864cfd12ffb0cc61e499a4c832d6
gaarangoa <gaarangoa@gmail.com>
2019-01-25 20:58:01 -0500

    random kmer length for training but hash is the same

--- src/Index.cpp
+++ src/Index.cpp
@@ -195,9 +195,10 @@ void Index::indexing(std::string finput, std::string output, int kmer, int label
 
             for (int i = ix; i <= Sl - k - 1; i += k)
             {
-                int ksizei = uni(rng);
+                int ksizei = ksize(rng);
                 ks = rProt.substr(i, k);
                 kmers[ks][prelabel] = true;
+                ks = rProt.substr(i, ksizei);
 
                 if (count_kmers % min_kmers == 0 && count_kmers > 0)
                 {
