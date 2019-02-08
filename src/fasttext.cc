21c0af37b2e10c3ccc5a2dd3236f9ff99d0766b7
gaarangoa <gaarangoa@gmail.com>
2019-02-08 16:35:58 -0500

    fix

--- src/fasttext.cc
+++ src/fasttext.cc
@@ -225,7 +225,7 @@ void FastText::printInfo(real progress, real loss)
     int eta = int(t / progress * (1 - progress) / args_->thread);
     int etah = eta / 3600;
     int etam = (eta - etah * 3600) / 60;
-    std::cout << std::fixed;
+    // std::cout << std::fixed;
     std::cout << "\rProgress: " << std::setprecision(1) << 100 * progress << "%"
               << "  words/sec/thread: " << std::setprecision(0) << wst << "  lr: " << std::setprecision(6) << lr << "  loss: " << std::setprecision(6) << loss << "  eta: " << etah << "h" << etam << "m " << std::flush;
 }
