45531545fcea1f20fd0fb35ca8801b4d4425f4a3
gustavo arango <gaarangoa@gmail.com>
2019-02-27 11:18:42 -0500

    update

--- src/fasttext.cc
+++ src/fasttext.cc
@@ -227,7 +227,7 @@ void FastText::printInfo(real progress, real loss)
     int etam = (eta - etah * 3600) / 60;
     std::cout << std::fixed;
     // std::cout << "\rProgress (%): " << std::setprecision(1) << 100 * progress << std::flush;
-    << "  words/sec/thread: " << std::setprecision(0) << wst << "  lr: " << std::setprecision(6) << lr << "  loss: " << std::setprecision(6) << loss << "  eta: " << etah << "h" << etam << "m " << std::flush;
+    std::cout << "  words/sec/thread: " << std::setprecision(0) << wst << "  lr: " << std::setprecision(6) << lr << "  loss: " << std::setprecision(6) << loss << "  eta: " << etah << "h" << etam << "m " << std::flush;
 }
 
 std::vector<int32_t> FastText::selectEmbeddings(int32_t cutoff) const
